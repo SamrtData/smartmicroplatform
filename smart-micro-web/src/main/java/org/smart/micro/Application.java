223bad0c37a8d2a5fa703d3978287a9bcab2cc68
jiangsida <jiangsida@longigroup.com>
2019-05-07 16:37:04 +0800

    初始化

---
+++ smart-micro-web/src/main/java/org/smart/micro/Application.java
@@ -0,0 +1,17 @@
+package org.smart.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author jiangsida
+ * @VERSION 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
